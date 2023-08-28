81313da5bd822509c67a95f6b6fa8ce3bdabcb96
karynaraujo <karyna.araujo@ccc.ufcg.edu.br>
2023-08-28 20:47:27 -0300

    teste_karyna

--- teste.hs
+++ teste.hs
@@ -1,10 +1,19 @@
+import Text.XHtml (input)
 dobro :: Int-> Int
 dobro x = x * 2
 
 triplo:: Int -> Int
 triplo x = 3 * x
 
+quadruplo:: Int -> Int
+quadruplo x = 4 * x
+
 par:: Int -> Bool
 par x
     | mod x 2 == 0 = True
     | otherwise = False
+
+main:: IO()
+main = do
+    input <- readLn::IO Int
+    print(quadruplo input)
