0a4c8719348a90bcc3f43b6d86f23f167263d657
larysamendes <larysamendes15@gmail.com>
2023-09-03 12:35:29 -0300

    "Model"

--- src/Model/Disciplina.hs
+++ src/Model/Disciplina.hs
@@ -1,7 +1,7 @@
 module Model.Disciplina where
     data Disciplina = Disciplina {
-        id :: Int
-        nome :: String
-        professor :: String
-        periodo :: String
+        id :: Int,
+        nome :: String,
+        professor :: String,
+        periodo :: String,
     } deriving (Show, Read)
