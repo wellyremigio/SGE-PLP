e70e4f2bc97f8feb84758a3aacc4b682cc23c0fc
Welly Remígio <welly.remigio5@gmail.com>
2023-09-13 17:36:02 -0300

    Método cadastrar JSON

--- plp-sge/haskell/src/DataBase/Gerenciador/AlunoGerenciador.hs
+++ plp-sge/haskell/src/DataBase/Gerenciador/AlunoGerenciador.hs
@@ -10,6 +10,14 @@ import Model.Aluno
 instance FromJSON Aluno
 instance ToJSON Aluno
 
+cadastrarAlunoJSON :: String -> Int -> String -> String -> IO ()
+cadastrarAlunoJSON jsonFilePath matricula nome senha = do
+    let aluno = Aluno matricula nome senha
+    let alunoList = (getAlunos jsonFilePath) ++ [aluno]
+
+    B.writeFile "../Temp.json" $ encode alunoList
+    removeFile jsonFilePath
+    renameFile "../Temp.json" jsonFilePath
 
 getAlunoByMatricula :: Int-> [Aluno] -> Aluno
 getAlunoByMatricula _ [] = Aluno (-1) ""
@@ -17,6 +25,18 @@ getAlunoByMatricula matriculaProcurada (x:xs)
  | (matricula x) == matriculaProcurada = x
  | otherwise = getAlunoByMatricula matriculaProcurada xs
 
+getAlunos :: FilePath -> IO [Aluno]
+ getAlunos path = do
+   let filePath = path </> "aluno.json"
+   conteudo <- B.readFile filePath
+   let alunos = fromMaybe [] (decode conteudo)
+   return alunos
+
+verificarLogin :: Int -> String -> IO Bool
+verificarLogin matricula senha = do
+ alunos <- getAlunos
+ let alunoEncontrado = find (\aluno -> matricula == matricula aluno && senha == senha aluno) alunos
+ return (isJust alunoEncontrado)
 
 
 
