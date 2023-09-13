1d97d131ed85322561f1822f8155ad052286c304
karynaraujo <karyna.araujo@ccc.ufcg.edu.br>
2023-09-13 18:28:53 -0300

    adicionando arquivo dos menus

--- plp-sge/haskell/src/Main.hs
+++ plp-sge/haskell/src/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 {-import DataBase.Gerenciador as BD--
 import Function.AlunoFunction as AlunoF
-import Function.ComentarioFunction as Comentario
+import Function.ComentarioFunction as ComentarioF
 import Function.DataFunction as DataF
 import Function.DisciplinaFunction as DisciplinaF
 import Function.GrupoFunction as GrupoF
@@ -22,5 +22,19 @@ import qualified Data.Time.Format as TimeFormat
 
 main:: IO()
 main = do
-    putStr "\n =========== Olá! Seja bem vindo ao SGE! ===========\n"
+    putStr "\n =========== Olá! Seja bem vindo ao SGE: Sistema de Gerenciamento de Estudos :D ===========\n"
     putStr "\n Escolha uma opção para começar a navegar no sistema: \n"
+    putStr "1. Login\n"
+    putStr "2. Cadastrar\n"
+    op <- readLn:: IO Int
+    opSelecionada op
+
+opSelecionada:: Int -> IO()
+opSelecionada op
+    | op == 1 = menuLogin
+    | op == 2 = menuCadastro
+    | otherwise = do
+        putStr "Ops! Entrada Inválida...\n"
+        main
+
+    
---
+++ plp-sge/haskell/src/Menus.hs
@@ -0,0 +1,21 @@
+module Menus where
+
+{-import DataBase.Gerenciador as BD--
+import Function.AlunoFunction as AlunoF
+import Function.ComentarioFunction as ComentarioF
+import Function.DataFunction as DataF
+import Function.DisciplinaFunction as DisciplinaF
+import Function.GrupoFunction as GrupoF
+import Function.LinksUteisFunction as LinksF
+import Function.ResumoFunction as ResumoF
+
+import Model.Aluno
+import Model.Comentario
+import Model.Data
+import Model.Disciplina
+import Model.Grupo
+import Model.LinksUteis
+import Model.Resumo-}
+
+import Data.Time.Clock ()
+import qualified Data.Time.Format as TimeFormat
