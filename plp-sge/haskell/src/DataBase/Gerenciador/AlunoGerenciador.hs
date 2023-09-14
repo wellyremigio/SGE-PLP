7de335fbf1c2879f04e917dd15c78d411dbb8b22
pedrohspi <pedro.pereira@ccc.ufcg.edu.br>
2023-09-13 23:31:46 -0300

    atualizacao

--- plp-sge/haskell/src/DataBase/Gerenciador/AlunoGerenciador.hs
+++ plp-sge/haskell/src/DataBase/Gerenciador/AlunoGerenciador.hs
@@ -4,8 +4,12 @@ import Data.Aeson
 import GHC.Generics
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as BC
-
+import Data.Maybe (fromMaybe)
+import System.Directory (renameFile)  -- Adicione esta linha
+import System.FilePath ((</>))
 import Model.Aluno
+import System.Directory (removeFile)
+
 
 instance FromJSON Aluno
 instance ToJSON Aluno
@@ -13,17 +17,20 @@ instance ToJSON Aluno
 salvarAlunoJSON :: String -> Int -> String -> String -> IO ()
 salvarAlunoJSON jsonFilePath matricula nome senha = do
     let aluno = Aluno matricula nome senha
-    let alunoList = (getAlunos jsonFilePath) ++ [aluno]
+    alunos <- getAlunosJSON jsonFilePath -- Obtém a lista de alunos do arquivo JSON
+    let alunoList = alunos ++ [aluno] -- Concatena o novo aluno à lista existente
 
     B.writeFile "../Temp.json" $ encode alunoList
     removeFile jsonFilePath
     renameFile "../Temp.json" jsonFilePath
 
-getAlunoByMatricula :: Int-> [Aluno] -> Aluno
-getAlunoByMatricula _ [] = Aluno (-1) ""
+
+getAlunoByMatricula :: Int -> [Aluno] -> Aluno
+getAlunoByMatricula _ [] = Aluno 0 "" ""
 getAlunoByMatricula matriculaProcurada (x:xs)
- | (matricula x) == matriculaProcurada = x
- | otherwise = getAlunoByMatricula matriculaProcurada xs
+    | matricula x == matriculaProcurada = x
+    |  otherwise = getAlunoByMatricula matriculaProcurada xs    
+    
 
 getAlunosJSON :: FilePath -> IO [Aluno]
 getAlunosJSON path = do
