27334c952bdc90d65a1fe24ab5f05096f2bccad9
Welly Remígio <welly.remigio5@gmail.com>
2023-10-18 19:41:48 -0300

    Começando o main

---
+++ plp-sge/prolog/main.pl
@@ -0,0 +1,18 @@
+
+
+prompt(Message, String):-
+    write(Message),
+    flush_output,
+    read_line_to_codes(user_input, Codes),
+    string_codes(String, Codes).
+
+main:-
+    writeln( '\n =========== Olá! Seja bem vindo ao SGE: Sistema de Gerenciamento de Estudos :D ===========\n'),
+    write('\n Escolha uma opção para começar a navegar no sistema: \n'),
+    write('1. Login\n'),
+    write('2. Cadastrar\n'),
+    write('3. Sair\n'),
+    prompt('-->', Input),
+    atom_number(Input, Opcao),
+    write('\n'),
+    opSelecionada(Opcao).
