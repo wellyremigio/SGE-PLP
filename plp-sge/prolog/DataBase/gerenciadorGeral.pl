dfacd73a78bc523630881b394664f1672bf90268
Viniciusjsbarbosa <viniciusjsbarbosa1@gmail.com>
2023-10-25 18:56:35 -0300

    gerenciadorGeral.pl comentarios

--- plp-sge/prolog/DataBase/gerenciadorGeral.pl
+++ plp-sge/prolog/DataBase/gerenciadorGeral.pl
@@ -1,22 +1,22 @@
+% Lê um arquivo JSON e carrega seus dados em uma estrutura Prolog.
 load_json_file(File, Data) :-
     open(File, read, Stream),
     json_read(Stream, Data),
     close(Stream).
 
-% Regra geral salvar no banco de dados uma estrutura JSON recebida como parâmetro
+% Salva no banco de dados uma estrutura JSON recebida como parâmetro
 save_json_file(File, Data) :-
     open(File, write, Stream),
     json_write(Stream, Data),
     close(Stream).
 
-% Regra para adicionar um elemento a um arquivo JSON
+% Adiciona um elemento a um arquivo JSON existente.
 save_object(File, Element) :-
     load_json_file(File, Data),
     New_Data = [Element | Data],
     save_json_file(File, New_Data).
 
-
-% Regras que extraem as informações das entidades já citadas do banco de dados
+% Extraem as informações das entidades já citadas do banco de dados
 extract_info_aluno(json([id=Id, nome=Nome, senha=Senha, disciplinas=Disciplinas]), Id, Nome, Senha, Disciplinas).
 extract_info_disciplina(json([id=Id, nome=Nome, professor=Professor, periodo=Periodo, resumos=Resumos, datas=Datas, links=Links]), Id, Nome, Professor, Periodo, Resumos, Datas, Links).
 extract_info_grupo(json([id=Id, nome=Nome, alunos=Alunos, disciplinas=Disciplinas, adm=Adm]), Id, Nome, Alunos, Disciplinas, Adm).
@@ -25,7 +25,7 @@ extract_info_link_util(json([id=Id, titulo=Titulo, url=URL, comentarios=Comentar
 extract_info_data(json([id=Id, titulo=Titulo, dataInicio=DataInicio, dataFim=DataFim, comentarios=Comentarios]), Id, Titulo, DataInicio, DataFim, Comentarios).
 extract_info_comentario(json([id=Id, idAluno=IdAluno, comentario=Comentario]), Id, IdAluno,Comentario).
 
-% Regra que generaliza a extração do ID das entidades do banco de dados já citadas
+% Generaliza a extração do ID das entidades do banco de dados já citadas
 % (Essa regra é importante para generalizar o uso da regra de busca por ID)
 extract_id_object('aluno', Head_Object, Object_Id) :- extract_info_aluno(Head_Object, Object_Id, _, _, _).
 extract_id_object('disciplina', Head_Object, Object_Id) :- extract_info_disciplina(Head_Object, Object_Id, _, _, _, _, _, _).
@@ -35,27 +35,26 @@ extract_id_object('link', Head_Object, Object_Id) :- extract_info_link_util(Head
 extract_id_object('data', Head_Object, Object_Id) :- extract_info_data(Head_Object, Object_Id, _, _, _, _).
 extract_id_object('comentario', Head_Object, Object_Id) :- extract_info_comentario(Head_Object, Object_Id, _, _).
 
-
-% Regra geral que busca qualquer entidade por ID
+% Busca qualquer entidade por ID
 % Caso a entidade não seja encontrada, -1 é retornado
-seach_id([], _, -1, _) :- !. % Caso não o objeto buscado não exista, -1 é retornado
+seach_id([], _, -1, _) :- !.
 seach_id([Head_Object|Tail], Id, Object, Type) :- 
     extract_id_object(Type, Head_Object, Object_Id),
     (Object_Id = Id -> Object = Head_Object; seach_id(Tail, Id, Object, Type)).
 
-% Regra que generaliza a busca de entidades por id
+% Obtém uma entidade por ID.
 get_object_by_id(File, Id, Object, Type):-
     load_json_file(File, Data),
     seach_id(Data, Id, Object, Type).
  
-% Regra geral que remove um elemento de uma lista recebida
+% Remove um elemento de uma lista recebida
 % e tem como resultado a lista sem o elemento
 remove_object([], _, []).
 remove_object([Header|Tail], Object, Final_Data):-
     remove_object(Tail, Object, Data),
     (Header = Object -> Final_Data = Data ; Final_Data = [Header | Data]).
 
-% Regra geral que unifica a remoção de objetos por ID
+% Remove um objeto por ID e atualiza o arquivo JSON.
 remove_object_by_id(File, Id, Type):-
     load_json_file(File, Data),
     seach_id(Data, Id, Object, Type),
